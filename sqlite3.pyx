3d740714b263191985da046ae47f9b81f51903dc
Ike Tohru <rokujyouhitomajp@gmail.com>
2015-11-03 08:52:21 +0900

    Refactoring sqlite3.pyx

--- sqlite3.pyx
+++ sqlite3.pyx
@@ -1,13 +1,16 @@
-import string
+cdef inline get_version():
+  version = "0.0.1"
+  return version
 
-_C_SQLITE_VERSION = SQLITE_VERSION
-_C_SQLITE_VERSION_NUMBER = SQLITE_VERSION_NUMBER
-
-cdef version_info_from_version(s):
+cdef inline version_info_from_version(s):
+  import string
   return tuple([int(x) for x in string.split(s, ".")])
 
-version = "0.0.1"
-version_info = version_info_from_version(version)
+cdef _C_VERSION = get_version()
+cdef _C_SQLITE_VERSION = SQLITE_VERSION
+cdef _C_SQLITE_VERSION_NUMBER = SQLITE_VERSION_NUMBER
 
 sqlite_version = _C_SQLITE_VERSION
 sqlite_version_info = version_info_from_version(sqlite_version)
+version = _C_VERSION
+version_info = version_info_from_version(version)
